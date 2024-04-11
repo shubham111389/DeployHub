fbc82909c425258a7e3e4760c1e8640d54d8262d
shubham annpurne <shubhamannpurne1029@gmail.com>
2024-04-11 11:51:20 +0530

    add a random file name function generator and git clone for specific gihub repo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import express from "express";
 import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils";
 
-const app= express();
-app.use (cors ());
+const app = express();
+app.use(cors())
+app.use(express.json());
 
-app.listen( 3000);
+// POSTMAN
+app.post("/deploy", async (req, res) => {
+    const repoUrl = req.body.repoUrl;
+    console.log( repoUrl);
+    const id = generate(); // asd12
+    await simpleGit().clone(repoUrl, `output/${id}`);
+
+    res.json({
+        id: id
+    })
+});
+
+app.listen(3000);
